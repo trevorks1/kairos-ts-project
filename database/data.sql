6ae87d5e318900b070c3170e111a3907fe88936f
xarsin <jdj.souza@gmail.com>
2021-01-04 15:59:28 -0600

    created initial inserts for the tables that have static values

---
+++ database/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO "activity_type" ("activity_name")
+VALUES ('Arts & Crafts'),
+('Canvassing'),
+('Cleaning'),
+('Dog Walking'),
+('Event Help'),
+('Gardening'),
+('Landscaping'),
+('Litter Pickup'),
+('Moving'),
+('Other'),
+('Packing'),
+('Painting'),
+('Reading'),
+('Sorting')
+
+INSERT INTO "ages" ("range")
+VALUES ('0-4'),
+('5-12'),
+('13-17'),
+('Adult'),
+('Adult 55+')
+
+INSERT INTO "causes" ("cause")
+VALUES ('Animals & Pets'),
+('Arts & Music'),
+('Children & Youth'),
+('Colleges & Universities'),
+('Domestic Violence'),
+('Education & Literacy'),
+('Environment'),
+('Food & Nutrition'),
+('Health & Substance Abuse'),
+('Healthcare'),
+('Homelessness & Housing'),
+('Immigrants & Refugees'),
+('Job Training & Employment'),
+('Media & Journalism'),
+('Re-entry & Criminal Justice'),
+('Seniors & Caregivers'),
+('Sports & Recreation')
+
+INSERT INTO "access_level" ("title", "access_level")
+VALUES ('Admin', 10),
+('Organization', 20),
+('Volunteer', 30)
