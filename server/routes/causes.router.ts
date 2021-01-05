bd8f1901744e22aad23d82accdbbaf62f87d95cc
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-05 16:21:19 -0600

    adding GET to retrieve events for a selected cause type

--- server/routes/causes.router.ts
+++ server/routes/causes.router.ts
@@ -10,7 +10,6 @@ const router: express.Router = express.Router();
 router.get(
   '/all',
   (req: Request, res: Response, next: express.NextFunction): void => {
-    // GET route code here
     const queryText: string =
       'SELECT "cause" from "causes" ORDER BY "cause" ASC;';
 
@@ -27,12 +26,30 @@ router.get(
 );
 
 /**
- * POST route template
+ * GET route to retrieve all events for a selected cause type
  */
-router.post(
-  '/',
+router.get(
+  '/select/:id',
   (req: Request, res: Response, next: express.NextFunction): void => {
-    // POST route code here
+    const causeId: number = parseInt(req.params.id);
+    const queryText: string = `SELECT * from "postings"
+    JOIN "organization" ON "postings".org_id = "organization".id
+    JOIN "org_causes" ON "organization".id = "org_causes".org_id
+    JOIN "causes" ON "org_causes".cause_id = "causes".id
+    WHERE "causes".id = $1;`;
+
+    pool
+      .query(queryText, [causeId])
+      .then((dbResponse) => {
+        res.send(dbResponse.rows);
+      })
+      .catch((err) => {
+        console.log(
+          'Could not get activities that match this cause type!',
+          err
+        );
+        res.sendStatus(500);
+      });
   }
 );
 
