c616b81278166c673196edbe2c8165f0e907f587
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-05 21:04:46 -0600

    adding activity.router to server.ts. adding GET route for all activity types

--- server/routes/activity.router.ts
+++ server/routes/activity.router.ts
@@ -1,16 +1,27 @@
-import { Request, Response } from 'express';
+import { query, Request, Response } from 'express';
 import express from 'express';
 import pool from '../modules/pool';
 
 const router: express.Router = express.Router();
 
 /**
- * GET route template
+ * GET route retrieves all activities
  */
 router.get(
-  '/',
+  '/all',
   (req: Request, res: Response, next: express.NextFunction): void => {
-    // GET route code here
+    const queryText: string =
+      'SELECT "activity_name" FROM "activity_type" ORDER BY "activity_name" ASC;';
+
+    pool
+      .query(queryText)
+      .then((dbResponse) => {
+        res.send(dbResponse.rows);
+      })
+      .catch((err) => {
+        console.log('Could not retrieve the list of activities!', err);
+        res.sendStatus(500);
+      });
   }
 );
 
@@ -24,4 +35,4 @@ router.post(
   }
 );
 
-module.exports = router;
+export default router;
--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import sessionMiddleware from './modules/session-middleware';
 import passport from './strategies/user.strategy';
 import userRouter from './routes/user.router';
+import activitiesRouter from './routes/activity.router';
 
 require('dotenv').config();
 
@@ -21,6 +22,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/activities', activitiesRouter);
 
 // Serve static files
 app.use(express.static('build'));
