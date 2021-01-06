e277bc25f142c55fdc603d9bf0cb59cdf04f85fe
Joey Heintz <joeheintz45@gmail.com>
2021-01-06 16:14:30 -0600

    complete user router

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -4,15 +4,11 @@ import rejectUnauthenticated from '../modules/authentication-middleware';
 import pool from '../modules/pool';
 import userStrategy from '../strategies/user.strategy';
 import { encryptPassword } from '../modules/encryption';
-
 const router: express.Router = express.Router();
-
 router.get('/', rejectUnauthenticated, (req: Request, res: Response): void => {
   res.send(req.user);
 });
-
 router.post(
-<<<<<<< HEAD
   '/register/vol',
   (req: Request, res: Response, next: express.NextFunction): void => {
     try {
@@ -25,7 +21,6 @@ router.post(
       const company: boolean | null = <boolean>req.body.company;
       const company_name: string | null = <string>req.body.company_name;
       const volunteer: boolean | null = <boolean>req.body.volunteer;
-
       const queryText: string = `INSERT INTO "user" (username, password, first_name, last_name, email_address, phone_number, 
     company, company_name, volunteer, active, access_level_id) 
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, true, 3) 
@@ -45,13 +40,10 @@ router.post(
         .then((result) => {
           const newUserId = result.rows[0].id;
           const userToActivity = [];
-
           for (let i = 0; i < req.body.activity_id.length; i++) {
             // looping through activity type array as they can select multiple
-
             const insertUserActivityQuery = `INSERT INTO "user_activity" ("activity_type_id", "user_id")
           VALUES ($1, $2);`;
-
             userToActivity.push(
               pool.query(insertUserActivityQuery, [
                 parseInt(req.body.activity_id[i]),
@@ -59,15 +51,12 @@ router.post(
               ])
             );
           }
-
           const userToAges = [];
           // for loop for age ranges query
           for (let i = 0; i < req.body.ages_id.length; i++) {
             // looping through ages_id array for the multi-select
-
             const insertAgeQuery = `INSERT INTO "user_ages" ("user_id", "ages_id")
           VALUES ($1, $2);`;
-
             userToAges.push(
               pool.query(insertAgeQuery, [
                 parseInt(newUserId),
@@ -85,26 +74,9 @@ router.post(
       console.log(`Error saving user to database: ${err}`);
       res.sendStatus(500);
     }
-=======
-  '/register',
-  (req: Request, res: Response, next: express.NextFunction): void => {
-    const username: string | null = <string>req.body.username;
-    const password: string | null = encryptPassword(req.body.password);
-
-    const queryText: string = `INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id`;
-    pool
-      .query(queryText, [username, password])
-      .then(() => res.sendStatus(201))
-      .catch((err) => {
-        console.log(`Error saving user to database: ${err}`);
-        res.sendStatus(500);
-      });
->>>>>>> 47abd9dbaf9885ace06d8e6e98c7a6153a216014
   }
 );
-
 router.post(
-<<<<<<< HEAD
   '/register/org',
   (req: Request, res: Response, next: express.NextFunction): void => {
     try {
@@ -117,7 +89,6 @@ router.post(
       const company: boolean | null = <boolean>req.body.company;
       const company_name: string | null = <string>req.body.company_name;
       const volunteer: boolean | null = <boolean>req.body.volunteer;
-
       const queryText: string = `INSERT INTO "user" (username, password, first_name, last_name, email_address, phone_number, 
     company, company_name, volunteer, active, access_level_id) 
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, false, 2) 
@@ -136,12 +107,10 @@ router.post(
         ])
         .then((result) => {
           const newUserId = result.rows[0].id;
-
           const insertOrgQuery = `INSERT INTO "organization" ("organization_name", "contact_title", "address", "mission",
           "summary", "website", "organization_type", "user_id")
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
           RETURNING id;`;
-
           pool
             .query(insertOrgQuery, [
               req.body.organization_name,
@@ -156,14 +125,11 @@ router.post(
             .then((resultOrg) => {
               const newOrgId = resultOrg.rows[0].id;
               const orgToCauses = [];
-
               // for loop for org_causes query
               for (let i = 0; i < req.body.causes.length; i++) {
                 // req.body.causes should be an array [1, 2, 3]
-
                 const insertCausesQuery = `INSERT INTO "org_causes" ("org_id", "cause_id")
               VALUES ($1, $2);`;
-
                 orgToCauses.push(
                   pool.query(insertCausesQuery, [
                     parseInt(newOrgId),
@@ -171,7 +137,6 @@ router.post(
                   ])
                 );
               }
-
               // Using Promise.all to require all results returned before moving on
               Promise.all(orgToCauses).then((result) => {
                 res.sendStatus(201);
@@ -184,20 +149,15 @@ router.post(
     }
   }
 );
-
 router.post(
-=======
->>>>>>> 47abd9dbaf9885ace06d8e6e98c7a6153a216014
   '/login',
   userStrategy.authenticate('local'),
   (req: Request, res: Response): void => {
     res.sendStatus(200);
   }
 );
-
 router.post('/logout', (req: Request, res: Response): void => {
   req.logout();
   res.sendStatus(200);
 });
-
 export default router;
