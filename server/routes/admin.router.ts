404e2df13437d956862794c27dc1122050d6380e
Trevor Sansalone <trevorksansalone@gmail.com>
2021-01-06 13:31:51 -0600

    created GET routes for admin requested and approved end points.

--- server/routes/admin.router.ts
+++ server/routes/admin.router.ts
@@ -8,9 +8,44 @@ const router: express.Router = express.Router();
  * GET list of organizations to be validated
  */
 router.get(
-  '/',
+  '/requested',
   (req: Request, res: Response, next: express.NextFunction): void => {
     // GET route code here
+    const queryText = `SELECT * FROM "user" 
+    JOIN "organization" ON "user".id = "organization".user_id 
+    WHERE "user".active = false;`;
+
+    pool
+      .query(queryText)
+      .then((dbResponse) => {
+        console.log(dbResponse);
+        res.send(dbResponse.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  }
+);
+
+router.get(
+  '/approved',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    // GET route code here
+    const queryText = `SELECT * FROM "user" 
+    JOIN "organization" ON "user".id = "organization".user_id 
+    WHERE "user".active = true;`;
+
+    pool
+      .query(queryText)
+      .then((dbResponse) => {
+        console.log(dbResponse);
+        res.send(dbResponse.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+      });
   }
 );
 
