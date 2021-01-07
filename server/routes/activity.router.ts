d0bd432978fb44af8f66ff1dc9af1dff34ca19e8
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-07 17:37:26 -0600

    adding DELETE route for volunteer user to remove a preferred activity

--- server/routes/activity.router.ts
+++ server/routes/activity.router.ts
@@ -97,6 +97,35 @@ router.post(
  * TODO!!
  * DELETE saved preferred activity for a logged in volunteer user
  */
+router.delete(
+  '/delete',
+  rejectUnauthenticated,
+  (req: any, res: Response, next: express.NextFunction): void => {
+    try {
+      // Promise.all me you love me
+
+      // data expected as an array of numbers! EVEN if it is a single number
+      const activityToDelete: number[] = req.body.activity_type_id;
+      // empty array that the queries can be pushed into
+      const arrayForPromise = [];
+      const volunteerId = req.user['id'];
+
+      for (let i = 0; i < activityToDelete.length; i++) {
+        const queryText: string = `DELETE FROM "user_activity" WHERE "user_id" = $1 AND "activity_type_id" = $2;`;
+        arrayForPromise.push(
+          pool.query(queryText, [volunteerId, activityToDelete[i]])
+        );
+      }
+
+      Promise.all(arrayForPromise).then(() => {
+        res.sendStatus(200);
+      });
+    } catch (err) {
+      console.log('could not delete preferred activities', err);
+      res.sendStatus(500);
+    }
+  }
+);
 
 /*
  * TODO!!
