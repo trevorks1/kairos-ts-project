07e8aa8a4d7056376612cdf3c8ec8554913654ab
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-08 13:14:00 -0600

    changing Home link to say Profile. stubbing in Browse link for non logged in user

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,7 +13,7 @@ const Nav = (props) => {
 
   if (props.store.user.id != null) {
     loginLinkData.path = '/user';
-    loginLinkData.text = 'Home';
+    loginLinkData.text = 'Profile';
   }
 
   return (
@@ -22,18 +22,21 @@ const Nav = (props) => {
         <h2 className="nav-title">Kairos</h2>
       </Link>
       <div className="nav-right">
+        {props.store.user.id == null && (
+          <Link className="nav-link">Browse</Link>
+        )}
         <Link className="nav-link" to={loginLinkData.path}>
           {/* Show this link if they are logged in or not,
-          but call this link 'Home' if they are logged in,
+          but call this link 'Profile' if they are logged in,
           and call this link 'Login / Register' if they are not */}
           {loginLinkData.text}
         </Link>
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.store.user.id && (
           <>
-            <Link className="nav-link" to="/info">
+            {/* <Link className="nav-link" to="/info">
               Info Page
-            </Link>
+            </Link> */}
             <LogOutButton className="nav-link" />
           </>
         )}
