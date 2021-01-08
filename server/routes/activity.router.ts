a2b834fcb7f14ee83e731861a10e068768e27edb
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-07 18:46:32 -0600

    removing admin related POST route

--- server/routes/activity.router.ts
+++ server/routes/activity.router.ts
@@ -26,32 +26,6 @@ router.get(
   }
 );
 
-/**
- * POST new activity type (for use by admin user)
- * TODO!!!
- * need to ADD rejectUnauthenticated once we are capable of testing with logged in admin user!!
- */
-router.post(
-  '/add',
-  (req: Request, res: Response, next: express.NextFunction): void => {
-    // new activity to be saved into DB
-    const newActivity: string = req.body.activity_name;
-
-    const queryText: string = `INSERT INTO "activity_type" (activity_name)
-    VALUES ($1);`;
-
-    pool
-      .query(queryText, [newActivity])
-      .then(() => {
-        res.sendStatus(201);
-      })
-      .catch((err) => {
-        console.log('Could not add new activity!', err);
-        res.sendStatus(500);
-      });
-  }
-);
-
 /*
  * TODO!!
  * GET saved preferred activities for a logged in volunteer user
