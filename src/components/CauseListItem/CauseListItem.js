8c12671468d2fcdfcbcaeea3f2d8c40c643cceba
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-10 18:19:42 -0600

    updating query to select all from causes table which allows access to id in click handler. yay

--- src/components/CauseListItem/CauseListItem.js
+++ src/components/CauseListItem/CauseListItem.js
@@ -5,13 +5,17 @@ import { Card, CardContent, CardActionArea } from '@material-ui/core';
 
 class CauseListItem extends Component {
   handleCauseClick = () => {
-    console.log('you clicked a cause!', this.props.cause.cause);
+    console.log(
+      'you clicked a cause!',
+      this.props.cause.cause,
+      this.props.cause.id
+    );
   };
 
   render() {
     return (
-      <Card onClick={this.handleCauseClick}>
-        <CardActionArea>
+      <Card>
+        <CardActionArea onClick={this.handleCauseClick}>
           <CardContent>
             <h4>{this.props.cause.cause}</h4>
           </CardContent>
