87efd2f295b424b7a0bf1c95013f2cb68a44ffbc
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-12 16:56:32 -0600

    adding view-activity/:id Route with stubbed in component ActivityDetailsPage

---
+++ src/pages/ActivityDetailsPage/ActivityDetailsPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ActivityDetailsPage extends Component {
+  render() {
+    return <h1>Activity Details!</h1>;
+  }
+}
+
+export default ActivityDetailsPage;
