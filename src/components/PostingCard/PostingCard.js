9c99644c625bda6eaeccff24dc959fd28e350f2d
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-12 16:05:01 -0600

    adding handlePostingClick - console logs the posting id

--- src/components/PostingCard/PostingCard.js
+++ src/components/PostingCard/PostingCard.js
@@ -8,20 +8,27 @@ import {
   CardHeader,
   CardContent,
   CardActions,
+  CardActionArea,
 } from '@material-ui/core';
 
 class PostingCard extends Component {
+  handlePostingClick = () => {
+    console.log(this.props.posting.id);
+  };
+
   render() {
     return (
       <Grid item xl={12}>
         {/* use avatar to display the number in top left corner */}
         <Card>
-          <CardHeader
-            avatar={<Avatar>{this.props.postingId + 1}</Avatar>} // adding 1 to postingId because array index starts at 0!
-            title={this.props.posting.organization_name}
-          />
-          <CardContent>{this.props.posting.description}</CardContent>
-          <CardActions>{this.props.posting.date_posted}</CardActions>
+          <CardActionArea onClick={this.handlePostingClick}>
+            <CardHeader
+              avatar={<Avatar>{this.props.postingId + 1}</Avatar>} // adding 1 to postingId because array index starts at 0!
+              title={this.props.posting.organization_name}
+            />
+            <CardContent>{this.props.posting.description}</CardContent>
+            <CardActions>{this.props.posting.date_posted}</CardActions>
+          </CardActionArea>
         </Card>
       </Grid>
     );
