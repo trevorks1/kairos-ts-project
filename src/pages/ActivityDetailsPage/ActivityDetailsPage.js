70501b8b904f969353b4addc8a837d4f99ea9a5c
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-12 17:34:56 -0600

    stubbing in formatting for the banner

--- src/pages/ActivityDetailsPage/ActivityDetailsPage.js
+++ src/pages/ActivityDetailsPage/ActivityDetailsPage.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
 
+// MATERIAL-UI
+import { Container, Box, Paper, Grid, Button } from '@material-ui/core';
+
 class ActivityDetailsPage extends Component {
   render() {
-    return <h1>Activity Details!</h1>;
+    return (
+      <Container>
+        <Paper>
+          <Grid container alignItems="center">
+            <Grid item xl={6}>
+              <Box ml={3}>
+                <Grid item>
+                  <h1>Activity Name</h1>
+                </Grid>
+              </Box>
+              <Box ml={3}>
+                <Grid item>
+                  <h3>Organization Name</h3>
+                </Grid>
+              </Box>
+            </Grid>
+            <Grid item xl={6}>
+              <Grid container alignItems="center" justify="space-evenly">
+                <Grid item>
+                  <Button variant="contained">BACK</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained">I WANT TO HELP</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    );
   }
 }
 
