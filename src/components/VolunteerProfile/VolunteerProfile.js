a045bdd4b1444ed0e140fa7269367631f013e011
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-14 10:01:39 -0600

    adding banner to volunteer profile page

--- src/components/VolunteerProfile/VolunteerProfile.js
+++ src/components/VolunteerProfile/VolunteerProfile.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+// MATERIAL-UI
+import { Container, Grid, Paper, Typography } from '@material-ui/core';
+
 class VolunteerProfile extends Component {
   render() {
     return (
-      <div>
-        <h1 id="welcome">
-          Welcome to your Volunteer Profile Page,{' '}
-          {this.props.store.user.username}!
-        </h1>
-        <p>Your ID is: {this.props.store.user.id}</p>
-      </div>
+      <Container>
+        <Grid container>
+          <Grid item lg={12}>
+            <Paper elevation={2}>
+              <Typography variant="h2" component="h2" align="center">
+                Thank you for being a volunteer
+              </Typography>
+              <Typography variant="h2" component="h2" align="center">
+                We appreciate you!
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     );
   }
 }
