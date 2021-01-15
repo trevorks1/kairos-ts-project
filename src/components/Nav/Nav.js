0145401a0e5fbdb36ce3a0a0e2e5696c65815657
Trevor Sansalone <trevorksansalone@gmail.com>
2021-01-15 08:39:28 -0600

    making a few changes to the navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -22,69 +22,61 @@ const Nav = (props) => {
       <Link to="/home">
         <h2 className="nav-title">Kairos</h2>
       </Link>
-      <Grid container spacing={12}>
-        <Grid item>
-          <div className="nav-right">
-            {/* the About link for organization seems pointless because with 
+
+      <div className="nav-right">
+        {/* the About link for organization seems pointless because with 
         auth redirect they end up getting directed to /user 
         we might have address this*/}
-            {props.store.user.access_level_id === 2 && (
-              <Button size="small" color="primary" to="/home">
-                About
-              </Button>
-            )}
-            {props.store.user.id == null && (
-              <Grid item xs={3}>
-                <Button
-                  size="small"
-                  color="secondary"
-                  variant="contained"
-                  to="/browse-causes"
-                >
-                  Browse
-                </Button>
-              </Grid>
-            )}
-            {props.store.user.access_level_id === 3 && (
-              <Button
-                size="small"
-                color="secondary"
-                variant="contained"
-                to="/browse-causes"
-              >
-                Browse
-              </Button>
-            )}
-            <ButtonGroup
-              justify="right"
-              size="small"
-              className="nav-link"
-              variant="contained"
-              to={loginLinkData.path}
-            >
-              <Button>Login</Button>
-              <Button>Register</Button>
-              {/* Show this link if they are logged in or not,
+        {props.store.user.access_level_id === 2 && (
+          <Button size="medium" color="primary" to="/home">
+            About
+          </Button>
+        )}
+        {props.store.user.id == null && (
+          <Button
+            size="medium"
+            color="secondary"
+            variant="contained"
+            to="/browse-causes"
+          >
+            Browse
+          </Button>
+        )}
+        {props.store.user.access_level_id === 3 && (
+          <Button
+            size="medium"
+            color="secondary"
+            variant="contained"
+            to="/browse-causes"
+          >
+            Browse
+          </Button>
+        )}
+        <Button
+          size="medium"
+          className="nav-link"
+          variant="contained"
+          to={loginLinkData.path}
+        >
+          {/* Show this link if they are logged in or not,
           but call this link 'Profile' if they are logged in,
           and call this link 'Login / Register' if they are not */}
-              {loginLinkData.text}
-            </ButtonGroup>
-            {/* Show the link to the info page and the logout button if the user is logged in */}
-            {props.store.user.id && (
-              <>
-                {/* <Link className="nav-link" to="/info">
+          {loginLinkData.text}
+        </Button>
+        {/* Show the link to the info page and the logout button if the user is logged in */}
+        {props.store.user.id && (
+          <>
+            {/* <Link className="nav-link" to="/info">
               Info Page
             </Link> */}
-                <LogOutButton className="nav-link" />
-              </>
-            )}
-            {/* Always show this link since the about page is not protected */}
-            {/* <Link className="nav-link" to="/about">
+            <LogOutButton className="nav-link" />
+          </>
+        )}
+        {/* Always show this link since the about page is not protected */}
+        {/* <Link className="nav-link" to="/about">
           About
         </Link> */}
-          </div>
-        </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
