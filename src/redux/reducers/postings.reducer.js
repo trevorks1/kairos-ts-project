34f19b4dc84c5728963880549904aa1d53785cf3
Brad Loudis <Bradley.Loudis@gmail.com>
2021-01-15 20:05:33 -0600

    adding postingsForVolunteerUser reducer

--- src/redux/reducers/postings.reducer.js
+++ src/redux/reducers/postings.reducer.js
@@ -11,6 +11,16 @@ const postingsForBrowsePage = (state = [], action) => {
   }
 };
 
+const postingsForVolunteerUser = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_POSTINGS_FOR_VOLUNTEER':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
 export default combineReducers({
   postingsForBrowsePage,
+  postingsForVolunteerUser,
 });
